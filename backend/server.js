aed74d4f95e72b9d0fe2f476220f41c4ae9345fb
zulkifalkhan <69888921+zulkifalkhan@users.noreply.github.com>
2022-05-27 18:23:01 +0500

    register with redux

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,21 @@ const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const Port = process.env.PORT || 5000
+var cors = require('cors')
 
 connectDB()
 
 const app = express()
 
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+ 
 app.use(express.json()) //to use req.body
 app.use(express.urlencoded({ extended: false }))
 
